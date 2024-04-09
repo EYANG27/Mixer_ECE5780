be9df55eb4f40e6cf7239c98803cea7e83182c71
shem-snow <78665574+shem-snow@users.noreply.github.com>
2024-04-09 10:05:43 -0600

    separated USART into a helper method and wrote a simple ADC

--- Cooler/Core/Src/main.c
+++ Cooler/Core/Src/main.c
@@ -20,8 +20,9 @@
 	* TODO: write documentation
 	*
 	*
-	*
-	*
+	* TODO:
+	*     Voltage for hot (warm) water: 
+	*     Voltage for cold (ice) water: 
 	*
 	*
 	*
@@ -41,8 +42,12 @@ void Transmit_Char(char c);
 void Transmit_String(char* str);
 void USART3_4_IRQHandler(void);
 void Process_TDR(char valve_ID, char action_ID);
+void Control_Valves(void);
 
+void Init_ADC(void);
+void Calibrate_and_start_ADC(void);
 void Init_Pins(void);
+void Sense_Temperature(void);
 
 /* Global variables -----------------------------------------------*/
 int GREEN = (1 << 9);
@@ -52,6 +57,8 @@ int RED = (1 << 6);
 
 volatile char received_byte;
 volatile uint8_t message_received_flag;
+static char valve_ID;
+static char action_ID;
 
 /**
   * @brief  The application entry point.
@@ -59,68 +66,26 @@ volatile uint8_t message_received_flag;
   */
 int main(void) {
 
-  // Reset, enable, and configure the clock and all peripherals.
+  // Reset the system.
   HAL_Init();
-  Init_LEDs();
-	Init_USART3();
   SystemClock_Config();
+	
+	// Configure the pins and each peripheral
+	Init_LEDs();
+	Init_USART3();
+	Init_ADC();
+	Init_Pins();
+	
 	
 	// Set initial conditions
 	message_received_flag = 0;
 	received_byte = '&'; // Initialized it to some junk that would never be processed.
 	
-	// Configure the pins we'll use.
-	Init_Pins();
-	
-	// Instantiate a null-terminated array to hold incoming messages.
-	char valve_ID;
-	char action_ID;
+	// Start the ADC.
+	Calibrate_and_start_ADC();
 
 	// Run
-  while (1) {
-		
-		// Display a prompt to the user to get two characters.
-		Transmit_String("\nSpecify the valve to open/close: ");
-		
-		// Receive the first character
-		while( !(USART3->ISR & (1<<5) )) {
-		}
-		received_byte = USART3->RDR & 0xFF;
-		Transmit_String("\nValve "); Transmit_Char(received_byte); Transmit_String(" is selected.");
-		
-		// If it's an accepted valve ID, save it and receive the second character.
-		if ( received_byte == '1' || received_byte == '2' || received_byte == '3') {
-			valve_ID = received_byte;
-			
-			// Recieve the second character
-			while( !(USART3->ISR & (1<<5) )) {
-			}
-			received_byte = USART3->RDR & 0xFF;
-			action_ID = received_byte;
-			
-			// If it's not a valid command, display an error message and abort the operation.
-			char* valve_op = "failed.";
-			if(received_byte == 'o')
-				valve_op = "\nOpening valve ";
-			else if(received_byte == 'c')
-				valve_op = "\nClosing valve ";
-			else{
-				Transmit_String("\n Invalid operation.");
-				continue;
-			}
-			
-			// Otherwise display what should be going on with the valve (Ex: "Closing valve 2").
-			Transmit_String(valve_op); Transmit_Char(received_byte);
-			
-			// Then perform the operation.
-			Process_TDR(valve_ID, action_ID);
-			continue;
-		}
-		
-		// Otherwise the input was invalid. Broadcast an error message and return to the beginning state.
-		Transmit_String("\nThat's is not a valid command, Try again."); // Error message for invalid character
-    
-  }
+  Sense_Temperature();
 }
 
 // _________________________________________________________ Peripheral and Pin Initializations __________________________________________________________________________________
@@ -200,6 +165,56 @@ void Init_USART3(void) {
 	NVIC_SetPriority(USART3_4_IRQn, 2);
 }
 
+void Init_ADC(void) {
+	
+	RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
+	
+	// PB6 is used for ADC because it can withstand 5v.
+	GPIOB->MODER |= 3 << (2*6); // Analog mode (11)
+	GPIOC->PUPDR &= ~(3 << (2*6)); // No pull-up, pull down (00)
+	ADC1->CHSELR |= (1 << 10 ); // Configure the pin for ADC conversion on channel 10
+	
+	// 8-bit resolution (10)
+	ADC1->CFGR1 |= (2 << 3);
+	ADC1->CFGR1 &= ~(1 << 3);
+	
+	// Continuous conversion mode
+	ADC1->CFGR1 |= (1 << 13);
+	
+	// Hardware triggers disabled (software-triggered only)
+	ADC1->CFGR1 &= ~(3 << 10);
+}
+
+void Calibrate_and_start_ADC(void) {
+	
+	// ___________________Calibrate (reference appendix A.7.1)___________________
+	// Calibration is initialted when ADEN = 1. So initialize it to zero/disable it.
+	if( (ADC1->CR & ADC_CR_ADEN) !=0)
+		ADC1->CR |= 1 << 1;
+	// Wait for the action to complete.
+	while ( (ADC1->CR & ADC_CR_ADEN) != 0) {
+	}
+	// Clear the DMA bit so 
+	ADC1->CFGR1 &= ~(1);
+	// Trigger the calibration in the control register
+	ADC1->CR |= (1 << 31);
+	// Wait for the action to complete.
+	while ( (ADC1->CR & ADC_CR_ADCAL) != 0) {
+	}
+	
+	
+	// ___________________ Enable Sequence code (reference appendix A.7.1)___________________
+	if( (ADC1->ISR & ADC_ISR_ADRDY) != 0)
+		ADC1->ISR |= ADC_ISR_ADRDY;
+	ADC1->CR |= ADC_CR_ADEN;
+	// Wait for the action to complete
+	while ( (ADC1->ISR & ADC_ISR_ADRDY) == 0 ) {
+	}
+	
+	// _____________________ Start _____________________
+	ADC1->CR |= (1 << 2);
+}
+
 void Init_Pins(void) {
 	
 	// Configure GPIOB 11, 12, and 13 to control the valves.
@@ -252,7 +267,51 @@ void USART3_4_IRQHandler(void) {
 	}
 }
 // _________________________________________________________ USART Handling __________________________________________________________________________________
-
+void Control_Valves(void) {
+	while(1) {
+		
+		// Display a prompt to the user to get two characters.
+		Transmit_String("\nSpecify the valve to open/close: ");
+		
+		// Receive the first character
+		while( !(USART3->ISR & (1<<5) )) {
+		}
+		received_byte = USART3->RDR & 0xFF;
+		Transmit_String("\nValve "); Transmit_Char(received_byte); Transmit_String(" is selected.");
+		
+		// If it's an accepted valve ID, save it and receive the second character.
+		if ( received_byte == '1' || received_byte == '2' || received_byte == '3') {
+			valve_ID = received_byte;
+			
+			// Recieve the second character
+			while( !(USART3->ISR & (1<<5) )) {
+			}
+			received_byte = USART3->RDR & 0xFF;
+			action_ID = received_byte;
+			
+			// If it's not a valid command, display an error message and abort the operation.
+			char* valve_op = "failed.";
+			if(received_byte == 'o')
+				valve_op = "\nOpening valve ";
+			else if(received_byte == 'c')
+				valve_op = "\nClosing valve ";
+			else{
+				Transmit_String("\n Invalid operation.");
+				continue;
+			}
+			
+			// Otherwise display what should be going on with the valve (Ex: "Closing valve 2").
+			Transmit_String(valve_op); Transmit_Char(received_byte);
+			
+			// Then perform the operation.
+			Process_TDR(valve_ID, action_ID);
+			continue;
+		}
+		
+		// Otherwise the input was invalid. Broadcast an error message and return to the beginning state.
+		Transmit_String("\nThat's is not a valid command, Try again."); // Error message for invalid character
+	}
+}
 /*
 * Does nothing while the transmit data register is empty.
 * When it's not empty, it transmits the character.
@@ -322,6 +381,27 @@ void Process_TDR(char valve_ID, char action_ID) {
 	}
 }
 
+
+// _________________________________________________________ Temperature Control __________________________________________________________________________________
+void Sense_Temperature(void) {
+	while(1) {
+		
+		// Start with everything turned off
+		GPIOC->ODR &= ~(RED | BLUE | GREEN | ORANGE);
+		
+		// Store the analog signal into a variable
+		int16_t temperature = ADC1->DR;
+		
+		if(temperature <= 100)
+			GPIOC->ODR |= RED;
+		else if(temperature < 150)
+			GPIOC->ODR |= GREEN;
+		else if(temperature < 300)
+			GPIOC->ODR |= BLUE;
+		else
+			GPIOC->ODR |= ORANGE;
+	}
+}
 // _________________________________________________________ System __________________________________________________________________________________
 /**
   * @brief System Clock Configuration
